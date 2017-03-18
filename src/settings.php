818b165585abb03b0a1bb224b72ea9d6cd9a5b31
James Hodgson <james@tdchodgy.co.uk>
2017-03-18 21:16:08 +0000

    Merging the merge

--- src/settings.php
+++ src/settings.php
@@ -14,5 +14,22 @@ return [
             'templates' => __DIR__ . '/../resources/views',
             'cache'     => getenv('APP_ENVIRONMENT') === 'prod' ? __DIR__ . '/../storage/cache/templates' : false,
         ],
+
+        'twitter' => [
+            'consumer' => [
+                'key' => getenv('TWITTER_CONSUMER_KEY'),
+                'secret' => getenv('TWITTER_CONSUMER_SECRET'),
+            ],
+            'access' => [
+                'token' => getenv('TWITTER_TOKEN'),
+                'secret' => getenv('TWITTER_TOKEN_SECRET'),
+            ],
+            'cache' => __DIR__ . '/../storage/cache/',
+        ],
+
+        'aylien' => [
+            'app_id' => getenv("AYLIEN_APP_ID"),
+            'key' => getenv("AYLIEN_KEY")
+        ]
     ],
 ];
