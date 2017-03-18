260bdb7178adde62b9133a46b43d4bca987cdce7
Adam Cooper <adam@networkpie.co.uk>
2017-03-18 21:16:20 +0000

    Mostly working webpack compile in place now.

--- resources/javascript/_main.js
+++ resources/javascript/_main.js
@@ -1,3 +1,18 @@
+// required core foundation files
+import { foundation } from 'foundation-sites/js/foundation.core';
+import 'foundation-sites/js/foundation.util.mediaQuery';
+
+// we need to attach the function we force-exported in the config
+// above to the jQuery object in use in this file
+$.fn.foundation = foundation;
+
+// ready to go
+//$(document).ready(function() {
+//    $(document).foundation();
+
+
+//});
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
