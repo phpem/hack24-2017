3de90b8a16ad5ec496560e5e825aee3487865adf
Adam Cooper <adam@networkpie.co.uk>
2017-03-18 23:15:05 +0000

    Switch to a more lightweight source maps.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    devtool: 'source-map',
+    devtool: 'eval', //'source-map',
     entry: [
         './resources/app.js'
     ],
